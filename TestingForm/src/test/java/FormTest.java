f312846246d2498835baa746aa7c421c12eecf20
AndryKr <yandryx@ya.ru>
2021-02-02 23:57:10 +0300

    Дополнил настройки

--- TestingForm/src/test/java/FormTest.java
+++ TestingForm/src/test/java/FormTest.java
@@ -3,10 +3,14 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 import org.junit.Test;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class FormTest {
 
     private WebDriver driver;
+    private WebDriverWait wait;
 
     @Test
     public void testGoogleSearch() throws InterruptedException {
@@ -14,7 +18,12 @@ public class FormTest {
         System.setProperty("webdriver.chrome.driver", "webdriver/chromedriver.exe");
 
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://www.google.com/");
+
+
         Thread.sleep(5000);  // Let the user actually see something!
         WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.sendKeys("ChromeDriver");
